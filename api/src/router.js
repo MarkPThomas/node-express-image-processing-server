466aa23a75fc7512bec43f1da35066a66686eff4
Mark P Thomas <markums@gmail.com>
2022-12-24 22:26:44 -0700

    Completed Module 3

--- api/src/router.js
+++ api/src/router.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const multer = require('multer');
+const path = require('path');
 
 
 const router = Router();
@@ -31,6 +32,11 @@ router.post('/upload', upload.single('photo'), (req, res) => {
   } else {
     res.status(201).json({ success: true });
   }
+})
+
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
+router.get('/photo-viewer', (req, res) => {
+  res.sendFile(photoPath);
 })
 
 module.exports = router;
